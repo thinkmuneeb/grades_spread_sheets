bc5753741e7dc122ce3df09c9821731e9b6f5458
Muneeb <thinkmuneeb@gmail.com>
2020-09-13 03:52:58 +0500

    absolute grading is implemented. doing good.

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,3 +2,8 @@ Problem 13/09/2020
 1. get an excel sheet
 2. convert marks to grades
 3. apply different techniques
+
+There is a text file with input policy, how many to assign A grades, min limit, max limit, min %, max % 
+and same for all other grades. A simple setting, a complex settings below.
+
+ 
--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
+def grade_absolute(score):
+    s = score
+    if s > 80:
+        return 'A'
+    elif s > 70:
+        return 'B'
+    elif s > 60:
+        return 'C'
+    elif s >= 50:
+        return 'D'
+    elif s < 50:
+        return 'F'
+
+def grade_relative():
+    print('realative')
+        
 def process_workbook(filename):
     wb = xl.load_workbook(filename)
     sheet = wb['Sheet1']
@@ -8,21 +24,21 @@ def process_workbook(filename):
     for row in range(5, sheet.max_row+1):
         cell = sheet.cell(row, 3)
         print(cell.value)
-        garde = cell.value * 0.9
-        corrected_price_cell = sheet.cell(row,4)
-        corrected_price_cell.value = corrected_price
-        
+        grade = grade_absolute(cell.value)
+        grade_cell = sheet.cell(row,4)
+        grade_cell.value = grade
+    
     values = Reference(sheet,
-                min_row=2,
+                min_row=5,
                 max_row= sheet.max_row, 
-                min_col=4,
-                max_col=4)
+                min_col=3,
+                max_col=3)
     chart = BarChart()
     chart.add_data(values)
-    sheet.add_chart(chart, 'e2')
+    sheet.add_chart(chart, 'j4')
 
     wb.save(filename)
         
 
-process_workbook('transactions.xlsx')
-print('done')
+process_workbook('marks_to_grades.xlsx')
+print('--------done')
